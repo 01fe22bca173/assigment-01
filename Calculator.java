5fd078d4766b5818562f60c38c54870c5f82eeb9
01fe22bca173 <156897555+01fe22bca173@users.noreply.github.com>
2024-02-13 10:19:59 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -25,7 +25,7 @@ class Calculator extends Child
     public static void main(String[] args) {
         Calculator calc= new Calculator();
         float operand_1 =10;
-        float operand_1 =10;
+        float operand_2 =10;
 
  Scanner sc= new Scanner(System.in);
  System.out.println("ENTER THE OPERAND 1");
